bae0f52fbd215656f3beec518b6d292bfd38eca7
Gabriel Lopes Bastos <73184283+G4brielLB@users.noreply.github.com>
2025-10-07 14:49:58 -0300

    Conta com Getters e composição com Cliente + git ignore - issue #5

--- Conta.java
+++ Conta.java
@@ -1,13 +1,40 @@
 public class Conta {
     private String numeroConta;
     private String agencia;
-    private String titular;
+    private Cliente titular;
     private double saldo;
 
-    public Conta(String numeroConta, String agencia, String titular) {
+    public Conta(String numeroConta, String agencia, Cliente titular) {
         this.numeroConta = numeroConta;
         this.agencia = agencia;
         this.titular = titular;
         this.saldo = 0.0;
     }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "numeroConta='" + numeroConta + '\'' +
+                ", agencia='" + agencia + '\'' +
+                ", titular=" + titular +
+                ", saldo=" + saldo +
+                '}';
+    }
+    
 }
